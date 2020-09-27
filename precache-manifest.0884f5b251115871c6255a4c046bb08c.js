c3b6440c7708ead7ecc4db417ecf2a9e8d124dfa
Skyler <skylerseo.dev@gmail.com>
2020-09-27 21:10:11 +0900

    Updates

---
+++ precache-manifest.0884f5b251115871c6255a4c046bb08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a52710e66eebfd6a40da38e90655a9",
+    "url": "/react-what-did-you-eat/index.html"
+  },
+  {
+    "revision": "feaed8fb627c13978bba",
+    "url": "/react-what-did-you-eat/static/css/main.5c17c0b0.chunk.css"
+  },
+  {
+    "revision": "1c5e1e379c58a202ceb7",
+    "url": "/react-what-did-you-eat/static/js/2.2c310ccc.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react-what-did-you-eat/static/js/2.2c310ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaed8fb627c13978bba",
+    "url": "/react-what-did-you-eat/static/js/main.8280486c.chunk.js"
+  },
+  {
+    "revision": "7c99e4b7df532d8940ed",
+    "url": "/react-what-did-you-eat/static/js/runtime-main.e7208ae0.js"
+  }
+]);
